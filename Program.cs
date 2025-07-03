9b7ea6795dd799ab0dbbf53e871305b6202238b5
tejas1154 <tejas.ramane@aurionpro.com>
2025-07-03 09:39:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StringManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = args[0];
+
+            string[] parts = input.Split(',');
+            if (parts.Length == 3)
+            {
+                Console.WriteLine("Developer name is : " + parts[0]);
+                Console.WriteLine("Company name is : " + parts[1]);
+                Console.WriteLine("Location is : " + parts[2]);
+            }
+            else
+                Console.WriteLine("Invalid input format");
+        }
+    }
+}
